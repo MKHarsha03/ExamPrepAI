3eb7b6c3b03560b8354ff9ff3db0b95e373901fd
MKHarsha03 <124070180+MKHarsha03@users.noreply.github.com>
2025-01-09 22:00:19 +0530

    Testing llm api's using groq. Test is close to success but seems to problem in answering questions

---
+++ api.py
@@ -0,0 +1,4 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.vectorstores import InMemoryVectorStore
