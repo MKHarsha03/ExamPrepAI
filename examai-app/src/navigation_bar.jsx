f2e9e700e82579c1398796a82d04874c654b0480
MKHarsha03 <124070180+MKHarsha03@users.noreply.github.com>
2025-01-10 16:37:25 +0530

    Still working on navigation, next would be making it responsive

--- examai-app/src/navigation_bar.jsx
+++ examai-app/src/navigation_bar.jsx
@@ -3,23 +3,23 @@ import Home from './home.jsx';
 import Contact from './contact.jsx';
 import About from './about.jsx';
 import './index.css';
+import logo from './assets/logo.png';
 
 export default function NavigationBar(){
 
   return(
-    <>
-    <BrowserRouter>
-    <nav className="navigation">
+    <div>
+      <img src={logo} className="logo"/>
+    <BrowserRouter className="navigation">
       <Link to="/home" className="navigation-elements">Home</Link>
       <Link to="/contact" className="navigation-elements">Contact Us</Link>
       <Link to="/about" className="navigation-elements">About</Link>  
-    </nav>
     <Routes>
       <Route exact path="/home" element={<Home/>}/>
       <Route path="/contact" element={<Contact/>}/>
       <Route path="/about" element={<About/>}/>
     </Routes>
     </BrowserRouter>
-    </>
+    </div>
   );
 }
